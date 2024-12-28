4f82af2f0e848cfbd2bada15b4ce34a4882cb32f
Swati <115008404+swatimry@users.noreply.github.com>
2024-12-28 19:53:05 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+# fibonacci.py
+
+def fibonacci(n):
+    """Return the Fibonacci sequence up to the nth number."""
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence[:n]
